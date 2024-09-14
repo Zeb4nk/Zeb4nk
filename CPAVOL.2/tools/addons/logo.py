298978eab541167af2a97964c33f141eac73822b
Zeb4nk <zebanggarage20@gmail.com>
2024-09-14 16:29:24 +0700

    Create logo.py

---
+++ CPAVOL.2/tools/addons/logo.py
@@ -0,0 +1,43 @@
+"""This module provides a function that prints the logo's application."""
+
+import os
+
+from colorama import Fore as F
+
+
+def show_logo() -> None:
+    """Print the application logo.
+
+    Args:
+        None
+
+    Returns:
+        None
+    """
+    logo = """"\033[5m"
+               ▒▒████████      ▒▒█████████      ▒▒████████
+              ▒▒██     ▒▒█      ▒▒██   ▒▒██      ▒▒██  ▒▒██
+             ▒▒██               ▒▒██   ▒▒██      ▒▒██  ▒▒██
+             ▒▒██               ▒▒██▒█████       ▒▒████████
+              ▒▒██     ▒▒█      ▒▒██             ▒▒██  ▒▒██
+               ▒▒████████      ▒▒███            ▒▒███  ▒▒██  V.02
+           
+           --------------- CYBER PEOPLE ATTACK ---------------
+ 
+ ====================================|
+ Mod --->>>> Abdullah Al Asad        |
+ for you CPA members.                |
+ This tool can detect existing sites |
+ ====================================|
+ """
+
+    print(f"{F.GREEN}{logo}")
+    print("├─── DOS TOOL")
+    print("├─── AVAILABLE METHODS")
+    print("├───├ LAYER 7: ")
+    print("    ├───>>>> - HTTP -")
+    print("    ├───>>>> - HTTP-PROXY -")
+    print("    ├───>>>> - SLOWLORIS -")
+    print("    ├───>>>> - SLOWLORIS-PROXY -")
+    print("    ├")
+        
